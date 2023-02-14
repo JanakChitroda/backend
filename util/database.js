200f14e0dd15c70bb361f648cfe1fa3ee86fd42c
JanakChitroda <chitrodajm@gmail.com>
2023-02-14 22:30:32 +0530

    Backend For R

---
+++ util/database.js
@@ -0,0 +1,27 @@
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
+
+let db;
+
+const mongoConnect = (callback) => {
+    MongoClient.connect('mongodb+srv://Janak:janak1234@cluster0.ruzadtt.mongodb.net/shop?retryWrites=true&w=majority')
+        .then(client => {
+            console.log('Connected!');
+            db = client.db();
+            callback();
+        })
+        .catch(err => {
+            console.log(err);
+            throw err;
+        });
+};
+
+const getDb = () => {
+    if (db) {
+        return db;
+    }
+    throw 'No database found!';
+};
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
